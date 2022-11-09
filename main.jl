9af5ab62160e6d0a2bc14b3391cd0c80bcab5de6
Sebastian Callh <sebastian.callh@gmail..com>
2022-11-09 14:28:55 +0100

    Add forecasting

--- main.jl
+++ main.jl
@@ -2,6 +2,7 @@ using DataFramesMeta
 using StatsPlots
 using GaussianProcesses
 using Random
+using Statistics
 
 plot_dir = "plots"
 isdir(plot_dir) || mkdir(plot_dir)
@@ -25,20 +26,24 @@ data_plt = @df df scatter(
 savefig(data_plt, joinpath(plot_dir, "data.png"))
 
 function fit_model(df)
-    t, y, var = df.t, df.value, first(df.variable)
+    t = df.t
+    y = df.value
+    signal = first(df.variable)
     kernel = SE(0.0, 0.0)
-    gp = GP(t, y, MeanZero(), kernel, log(0.5))
+    gp = GP(t, y, MeanConst(mean(y)), kernel, log(0.5))
     optimize!(gp)
-    return gp, t, y, var
+    return gp, t, y, signal
 end
 
-function plot_fit((gp, t, y, signal))
-    μ, Σ = predict_y(gp, t)
-    plt = plot(t, μ, ribbon=2*sqrt.(Σ), label=nothing)
+function plot_fit((gp, t, y, signal); forecast=10)
+    t_pred = vcat(t, t[end]:t[end] + forecast)
+    μ, Σ = predict_y(gp, t_pred)
+    plt = plot(t_pred, μ, ribbon=2*sqrt.(Σ), label=nothing)
     plt = scatter!(plt, t, y, title=signal, label=nothing)
     return plt
 end
 
+N = length(unique(df.variable))
 plots = combine(groupby(df, :variable), AsTable(:) => plot_fit ∘ fit_model => :plot)
 fit_plt = plot(plots.plot..., layout=(N, 1), size=(600, 200N))
 savefig(fit_plt, joinpath(plot_dir, "model_fit.png"))
